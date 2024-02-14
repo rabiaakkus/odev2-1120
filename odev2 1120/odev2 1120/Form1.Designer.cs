bb810dfbcf635c0f704710fe3e649ad258fa3b13
rabiaakkus <145451092+rabiaakkus@users.noreply.github.com>
2024-02-14 13:44:38 +0300

    Add files via upload

---
+++ odev2 1120/odev2 1120/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace odev2_1120
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumara = new System.Windows.Forms.TextBox();
+            this.btnEkleSayi = new System.Windows.Forms.Button();
+            this.lbSayi = new System.Windows.Forms.ListBox();
+            this.lbMetin = new System.Windows.Forms.ListBox();
+            this.btnEkleMetin = new System.Windows.Forms.Button();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbNesne = new System.Windows.Forms.ListBox();
+            this.btnEkleNesne = new System.Windows.Forms.Button();
+            this.txtNumaraN = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAdSoyadN = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numara";
+            // 
+            // txtNumara
+            // 
+            this.txtNumara.Location = new System.Drawing.Point(76, 51);
+            this.txtNumara.Name = "txtNumara";
+            this.txtNumara.Size = new System.Drawing.Size(100, 20);
+            this.txtNumara.TabIndex = 1;
+            // 
+            // btnEkleSayi
+            // 
+            this.btnEkleSayi.Location = new System.Drawing.Point(76, 77);
+            this.btnEkleSayi.Name = "btnEkleSayi";
+            this.btnEkleSayi.Size = new System.Drawing.Size(100, 29);
+            this.btnEkleSayi.TabIndex = 2;
+            this.btnEkleSayi.Text = "Sayı ekle";
+            this.btnEkleSayi.UseVisualStyleBackColor = true;
+            this.btnEkleSayi.Click += new System.EventHandler(this.btnEkleSayi_Click);
+            // 
+            // lbSayi
+            // 
+            this.lbSayi.FormattingEnabled = true;
+            this.lbSayi.Location = new System.Drawing.Point(56, 112);
+            this.lbSayi.Name = "lbSayi";
+            this.lbSayi.Size = new System.Drawing.Size(120, 199);
+            this.lbSayi.TabIndex = 3;
+            // 
+            // lbMetin
+            // 
+            this.lbMetin.FormattingEnabled = true;
+            this.lbMetin.Location = new System.Drawing.Point(234, 112);
+            this.lbMetin.Name = "lbMetin";
+            this.lbMetin.Size = new System.Drawing.Size(120, 199);
+            this.lbMetin.TabIndex = 7;
+            // 
+            // btnEkleMetin
+            // 
+            this.btnEkleMetin.Location = new System.Drawing.Point(254, 77);
+            this.btnEkleMetin.Name = "btnEkleMetin";
+            this.btnEkleMetin.Size = new System.Drawing.Size(100, 29);
+            this.btnEkleMetin.TabIndex = 6;
+            this.btnEkleMetin.Text = "Metin Ekle";
+            this.btnEkleMetin.UseVisualStyleBackColor = true;
+            this.btnEkleMetin.Click += new System.EventHandler(this.btnEkleMetin_Click);
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(254, 51);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyad.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ad Soyad";
+            // 
+            // lbNesne
+            // 
+            this.lbNesne.FormattingEnabled = true;
+            this.lbNesne.Location = new System.Drawing.Point(427, 160);
+            this.lbNesne.Name = "lbNesne";
+            this.lbNesne.Size = new System.Drawing.Size(120, 199);
+            this.lbNesne.TabIndex = 11;
+            // 
+            // btnEkleNesne
+            // 
+            this.btnEkleNesne.Location = new System.Drawing.Point(427, 112);
+            this.btnEkleNesne.Name = "btnEkleNesne";
+            this.btnEkleNesne.Size = new System.Drawing.Size(100, 29);
+            this.btnEkleNesne.TabIndex = 10;
+            this.btnEkleNesne.Text = "Nesne ekle";
+            this.btnEkleNesne.UseVisualStyleBackColor = true;
+            this.btnEkleNesne.Click += new System.EventHandler(this.btnEkleNesne_Click);
+            // 
+            // txtNumaraN
+            // 
+            this.txtNumaraN.Location = new System.Drawing.Point(427, 51);
+            this.txtNumaraN.Name = "txtNumaraN";
+            this.txtNumaraN.Size = new System.Drawing.Size(100, 20);
+            this.txtNumaraN.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(377, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Numara";
+            // 
+            // txtAdSoyadN
+            // 
+            this.txtAdSoyadN.Location = new System.Drawing.Point(427, 82);
+            this.txtAdSoyadN.Name = "txtAdSoyadN";
+            this.txtAdSoyadN.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyadN.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(368, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Ad Soyad";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAdSoyadN);
+            this.Controls.Add(this.lbNesne);
+            this.Controls.Add(this.btnEkleNesne);
+            this.Controls.Add(this.txtNumaraN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbMetin);
+            this.Controls.Add(this.btnEkleMetin);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbSayi);
+            this.Controls.Add(this.btnEkleSayi);
+            this.Controls.Add(this.txtNumara);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumara;
+        private System.Windows.Forms.Button btnEkleSayi;
+        private System.Windows.Forms.ListBox lbSayi;
+        private System.Windows.Forms.ListBox lbMetin;
+        private System.Windows.Forms.Button btnEkleMetin;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbNesne;
+        private System.Windows.Forms.Button btnEkleNesne;
+        private System.Windows.Forms.TextBox txtNumaraN;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAdSoyadN;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ odev2 1120/odev2 1120/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev2_1120
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> numaralarList = new List<int>();
+        List<string> adsoyadList = new List<string>();
+        List<Ogrenciler> ogrencilerList = new List<Ogrenciler>();
+        private void btnEkleSayi_Click(object sender, EventArgs e)
+        {
+            numaralarList.Add(int.Parse(txtNumara.Text));
+            lbSayi.Items.Clear();
+            foreach (var item in numaralarList)
+            {
+                lbSayi.Items.Add(item);
+            }
+        }
+
+        private void btnEkleNesne_Click(object sender, EventArgs e)
+        {
+            Ogrenciler ogrenci = new Ogrenciler();
+            ogrenci.Numara = int.Parse(txtNumaraN.Text);
+            ogrenci.AdSoyad = txtAdSoyadN.Text;
+            ogrencilerList.Add(ogrenci);
+            lbNesne.Items.Clear();
+            foreach (var item in ogrencilerList)
+            {
+                lbNesne.Items.Add(item.Numara +""+item.AdSoyad);
+            }
+        }
+
+        private void btnEkleMetin_Click(object sender, EventArgs e)
+        {
+            adsoyadList.Add(string.Format(txtAdSoyad.Text));
+            lbMetin.Items.Clear();
+            foreach (var item in adsoyadList)
+            {
+                lbMetin.Items.Add(item);
+            }
+        }
+    }
+}
